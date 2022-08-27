b1277bf9c179be2703a26a88e397e0bb86d0df45
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-08-27 09:49:39 -0400

    add hover state/MouseOver listeners in Work for each project to fix bug with :hover deactivating while cursor is over <a> tags, add links to Github for all projects

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import pill from '../public/pill.svg'
 import socialScreenshot from '../public/socialScreenshot.png'
 import dancingScreenshot from '../public/dancingScreenshot.png'
@@ -7,6 +8,10 @@ import Image from 'next/image'
 import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function Work() {
+	const [topLeftHovered, setTopLeftHovered] = useState(false)
+	const [topRightHovered, setTopRightHovered] = useState(false)
+	const [bottomLeftHovered, setBottomLeftHovered] = useState(false)
+	const [bottomRightHovered, setBottomRightHovered] = useState(false)
 	const { scrollYProgress } = useScroll()
 	const opacityTransform = useTransform(
 		scrollYProgress,
@@ -68,88 +73,143 @@ export default function Work() {
 						DaisyUI
 					</p>
 					<div
-						id='gridSection1'
-						className='relative overflow-hidden w-full h-full rounded-[3rem] border-8 border-transparent col-start-1 col-span-2 row-start-2 row-span-5 shadow-thicc hover:bg-black hover:border-[#FF5B23]'
+						className='gridSection relative overflow-hidden w-full h-full rounded-[3rem] border-8 border-transparent col-start-1 col-span-2 row-start-2 row-span-5 shadow-thicc'
+						style={{
+							background: topLeftHovered && 'black',
+							border: topLeftHovered && '8px solid #FF5B23',
+						}}
+						onMouseEnter={() => {
+							setTopLeftHovered(true)
+						}}
+						onMouseLeave={() => {
+							setTopLeftHovered(false)
+						}}
 					>
 						<div className='p-4 flex flex-col justify-evenly items-center w-full h-full text-white'>
+							<Image
+								src={socialScreenshot}
+								alt='a Social Media app for pet owners'
+								layout='fill'
+								className='object-cover object-center'
+								style={{ opacity: topLeftHovered && '10%' }}
+							/>
 							<p className='projectTitle text-4xl'>PetSocial</p>
 							<p className='text-lg'>
 								Media sharing for pet owners and pet lovers
 							</p>
-							<div className='flex justify-evenly w-full'>
-								<p className='text-sm'>Github</p>
+							<div className='projectDetails flex justify-evenly w-full'>
+								<a
+									className='text-sm'
+									href='https://github.com/julianlk522/pet-social-media'
+								>
+									Github
+								</a>
+
 								<p className='text-sm'>Live demo</p>
 							</div>
 						</div>
-						<Image
-							src={socialScreenshot}
-							alt='a Social Media app for pet owners'
-							layout='fill'
-							className='object-cover object-center hover:opacity-10'
-						/>
 					</div>
 					<div
-						id='gridSection2'
-						className='relative w-full h-full row-start-2 row-span-5 overflow-hidden rounded-[3rem] border-8 border-transparent shadow-thicc hover:bg-black hover:border-[#FFACC6]'
+						className='gridSection relative w-full h-full row-start-2 row-span-5 overflow-hidden rounded-[3rem] border-8 border-transparent shadow-thicc'
+						style={{
+							background: topRightHovered && 'black',
+							border: topRightHovered && '8px solid #FFACC6',
+						}}
+						onMouseEnter={() => {
+							setTopRightHovered(true)
+						}}
+						onMouseLeave={() => {
+							setTopRightHovered(false)
+						}}
 					>
 						<div className='p-4 flex flex-col justify-evenly items-center w-full h-full text-white'>
+							<Image
+								src={typingScreenshot}
+								alt='a React-based typing game [add more later]'
+								className='object-cover object-left hover:opacity-20'
+								layout='fill'
+								style={{ opacity: topRightHovered && '20%' }}
+							/>
 							<p className='projectTitle text-2xl'>
 								Timed Typing Game
 							</p>
-							<div className='flex justify-evenly w-full'>
-								<p>Github</p>
+							<div className='projectDetails flex justify-evenly w-full'>
+								<a href='https://github.com/julianlk522/click-game-react-port'>
+									Github
+								</a>
 								<p>Live</p>
 							</div>
 						</div>
-						<Image
-							src={typingScreenshot}
-							alt='a React-based typing game [add more later]'
-							className='object-cover object-left hover:opacity-20'
-							layout='fill'
-						/>
 					</div>
 					<div
-						id='gridSection3'
-						className='relative w-full h-full row-start-7 row-span-5 overflow-hidden rounded-[3rem] border-8 border-transparent border-opacity-50 shadow-thicc hover:bg-black hover:border-[#00d8ff]'
+						className='gridSection relative w-full h-full row-start-7 row-span-5 overflow-hidden rounded-[3rem] border-8 border-transparent border-opacity-50 shadow-thicc'
+						style={{
+							background: bottomLeftHovered && 'black',
+							border: bottomLeftHovered && '8px solid #00D8FF',
+						}}
+						onMouseEnter={() => {
+							setBottomLeftHovered(true)
+						}}
+						onMouseLeave={() => {
+							setBottomLeftHovered(false)
+						}}
 					>
 						<div className='p-4 flex flex-col justify-evenly items-center w-full h-full text-white'>
+							<Image
+								src={dancingScreenshot}
+								alt='a simple game to test your reactions and accuracy usig the mouse'
+								className='object-cover object-left-bottom'
+								layout='fill'
+								style={{ opacity: bottomLeftHovered && '20%' }}
+							/>
 							<p className='projectTitle text-2xl'>
 								Dancing Button
 							</p>
-							<div className='flex justify-evenly w-full'>
-								<p>Github</p>
+							<div className='projectDetails flex justify-evenly w-full'>
+								<a href='https://github.com/julianlk522/dancing-button'>
+									Github
+								</a>
 								<p>Live</p>
 							</div>
 						</div>
-						<Image
-							src={dancingScreenshot}
-							alt='a simple game to test your reactions and accuracy usig the mouse'
-							className='object-cover object-left-bottom hover:opacity-20'
-							layout='fill'
-						/>
 					</div>
 					<div
-						id='gridSection4'
-						className='relative overflow-hidden w-full h-full rounded-[3rem] border-8 border-transparent col-start-2 col-span-2 row-start-7 row-span-5 shadow-thicc hover:bg-black hover:border-[#FF5B23]'
+						className='gridSection relative overflow-hidden w-full h-full rounded-[3rem] border-8 border-transparent col-start-2 col-span-2 row-start-7 row-span-5 shadow-thicc'
+						style={{
+							background: bottomRightHovered && 'black',
+							border: bottomRightHovered && '8px solid #FF5B23',
+						}}
+						onMouseEnter={() => {
+							setBottomRightHovered(true)
+						}}
+						onMouseLeave={() => {
+							setBottomRightHovered(false)
+						}}
 					>
 						<div className='p-4 flex flex-col justify-evenly items-center w-full h-full text-white'>
+							<Image
+								src={chatScreenshot}
+								alt='a chat app for messaging your friends'
+								className='object-cover object-left-bottom'
+								layout='fill'
+								style={{ opacity: bottomRightHovered && '10%' }}
+							/>
 							<p className='projectTitle text-4xl'>
 								Chat Station
 							</p>
 							<p className='text-lg'>
 								Quickly and conveniently message your friends
 							</p>
-							<div className='flex justify-evenly w-full'>
-								<p className='text-sm'>Github</p>
+							<div className='projectDetails flex justify-evenly w-full'>
+								<a
+									href='https://github.com/julianlk522/chat-app'
+									className='text-sm'
+								>
+									Github
+								</a>
 								<p className='text-sm'>Live demo</p>
 							</div>
 						</div>
-						<Image
-							src={chatScreenshot}
-							alt='a chat app for messaging your friends'
-							className='object-cover object-left-bottom hover:opacity-10'
-							layout='fill'
-						/>
 					</div>
 					<p className='mt-[-1rem] col-start-1 row-start-12 row-span-1 text-xs z-[1]'>
 						Javascript, HTML, CSS
