707143a0141c2cd3d05bae8e3b62d55e9413f84e
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-11-24 11:44:55 -0500

    add Navbar slide in/out effect and add white bg in lightMode, move animation settings to variants

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,15 +9,22 @@ export default function Navbar({
 	showDropdown,
 }) {
 	const navVariants = {
+		hidden: {
+			y: -48,
+			opacity: 0,
+			boxShadow: 'none',
+		},
 		darkMode: {
-			opacity: 1,
-			backgroundColor: 'rgba(51, 65, 85, 0.75)',
+			opacity: 0.75,
+			backgroundColor: 'rgb(51, 65, 85)',
 			boxShadow: '0px 0px 10px rgba(153, 153, 153, 0.5)',
+			y: 0,
 		},
 		lightMode: {
 			opacity: 1,
-			backgroundColor: 'rgba(51, 65, 85, 0)',
-			boxShadow: '0px 0px 0px rgba(153, 153, 153, 0)',
+			backgroundColor: 'rgb(255,255,255)',
+			boxShadow: 'none',
+			y: 0,
 		},
 	}
 
@@ -28,9 +35,13 @@ export default function Navbar({
 					id='navbar'
 					className='fixed top-0 left-0 z-[1] flex h-16 w-full items-center justify-end px-16 dark:text-white md:justify-evenly'
 					variants={navVariants}
-					initial={false}
+					initial='hidden'
 					animate={darkMode ? 'darkMode' : 'lightMode'}
-					exit={{ opacity: 0 }}
+					exit='hidden'
+					transition={{
+						duration: 0.5,
+						type: 'tween',
+					}}
 				>
 					<HomeButtonLogo darkMode={darkMode} />
 
